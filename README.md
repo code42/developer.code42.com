e601c2b3bdbeacdfebee2ca171582e6408fa0e51
Alan Grgic <alan.grgic@code42.com>
2020-11-12 14:42:14 -0600

    build api docs

---
+++ README.md
@@ -0,0 +1,17 @@
+# Code42 Developer Portal
+
+*NOTE: This site / repo is a work in progress. The APIs documented by this project are likely to not work and / or change in the future. Do not use or depend on them until this project has officially launched.*
+
+To build, execute:
+
+```bash
+./build.sh
+```
+
+Once built, the site can be run locally using a local web server serving the current working directory, such as python's built in http module:
+
+```bash
+python -m http.server 1337
+```
+
+Then, open your browser to `localhost:1337/publish` (or whatever port you chose) to explore the site (on the live site, `publish` will be excluded from the url).
--- build.sh
+++ build.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
+set -eu
+
 BUILD=build
 DOCS=$BUILD/docs
 OUT=$DOCS/out
+PUBLISH=publish
+SANDBOX=$PUBLISH/sandbox
 
-DOCS_SERVER="http://localhost:5000"
+DOCS_SERVER="https://default-api.core-int.cloud.code42.com"
 
 rm -rf $BUILD
+rm -f $SANDBOX/code42api.json
 mkdir -p $OUT
 
-DOCS_ENDPOINTS=$(curl $DOCS_SERVER/api/docs-index | jq -r .services[].location)
+# get the list of all swagger file locations
+DOCS_ENDPOINTS=$(curl $DOCS_SERVER/api/docs-index/v1 | jq -r .services[].location)
 printf "%s\n" "$DOCS_ENDPOINTS" > $BUILD/docs_endpoints.txt
 sed -i -e "s~^~$DOCS_SERVER~" $BUILD/docs_endpoints.txt
+
+# download all of the swagger files into the $DOCS directory
 wget -i $BUILD/docs_endpoints.txt -P $DOCS
 
+# grab all the paths and definitions from every swagger file that was found
 jq -s '.[].paths' $DOCS/* | jq -s add > $BUILD/paths.json
 jq -s '.[].definitions' $DOCS/* | jq -s add > $BUILD/defs.json
 
-jq '.paths |= $paths' --argfile paths $BUILD/paths.json template.json > $BUILD/merged_paths.json
+# append all of the found paths and definitions into a single swagger file
+jq '.paths |= $paths' --argfile paths $BUILD/paths.json swagger_template.json > $BUILD/merged_paths.json
 jq '.definitions |= $defs' --argfile defs $BUILD/defs.json $BUILD/merged_paths.json > $OUT/code42api.json
+
+# put the merged file into the directory for publising
+cp $OUT/code42api.json $SANDBOX
