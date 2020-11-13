fa668323f82d4531a9e66787b9480c4453b57dba
Alan Grgic <alan.grgic@code42.com>
2020-11-13 13:58:14 -0600

    Build and deploy API docs (#1)

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -eu
+
+BUILD=build
+DOCS=$BUILD/docs
+OUT=$DOCS/out
+PUBLISH=publish
+SANDBOX=$PUBLISH/sandbox
+
+DOCS_SERVER="https://default-api.core-int.cloud.code42.com"
+
+rm -rf $BUILD
+rm -f $SANDBOX/code42api.json
+mkdir -p $OUT
+
+# get the list of all swagger file locations
+DOCS_ENDPOINTS=$(curl $DOCS_SERVER/api/docs-index/v1 | jq -r .services[].location)
+printf "%s\n" "$DOCS_ENDPOINTS" > $BUILD/docs_endpoints.txt
+sed -i -e "s~^~$DOCS_SERVER~" $BUILD/docs_endpoints.txt
+
+# download all of the swagger files into the $DOCS directory
+echo "fetching swagger documents..."
+wget -i $BUILD/docs_endpoints.txt -P $DOCS
+
+# grab all the paths and definitions from every swagger file that was found
+echo "building unified swagger document..."
+jq -s '.[].paths' $DOCS/* | jq -s add > $BUILD/paths.json
+jq -s '.[].definitions' $DOCS/* | jq -s add > $BUILD/defs.json
+
+# append all of the found paths and definitions into a single swagger file
+jq '.paths |= $paths' --argfile paths $BUILD/paths.json swagger_template.json > $BUILD/merged_paths.json
+jq '.definitions |= $defs' --argfile defs $BUILD/defs.json $BUILD/merged_paths.json > $OUT/code42api.json
+
+# put the merged file into the directory for publishing
+cp $OUT/code42api.json $SANDBOX
+echo "Done."
