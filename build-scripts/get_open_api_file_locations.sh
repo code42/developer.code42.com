b527126820c9cf2cfcb04a6f5ac5d8c390dd2c1a
Juliya Smith <juliya.smith@code42.com>
2020-12-17 14:20:39 +0000

    build improvs

--- build-scripts/get_open_api_file_locations.sh
+++ build-scripts/get_open_api_file_locations.sh
@@ -5,7 +5,11 @@ set -euo pipefail
 main () {
   local docs_server="${1:?Missing param docs_server at index 1.}"
   local docs_src="${2:?Missing param docs_src at index 2.}"
-  local docs_endpoints=$(curl ${docs_server}/api/docs-index/v1 | jq -r .services[].location)
+  local docs_endpoints=$(curl -s ${docs_server}/api/docs-index/v1 | jq -r .services[].location)
+  if [[ -z "${docs_endpoints}" ]]; then
+    echo "Failed getting endpoints"
+    exit 1
+  fi
   printf "%s\n" "${docs_endpoints}" > ${docs_src}/docs_endpoints.txt
   sed -i -e "s~^~${docs_server}~" ${docs_src}/docs_endpoints.txt
 }
