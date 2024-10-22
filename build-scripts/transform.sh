eb7d86e41337a7e623cbc00a86769b961c4dcaa4
Ben Johnson <34320012+johnsonjben@users.noreply.github.com>
2024-10-22 11:54:57 -0400

    INTEG-2668: add logs (#126)

--- build-scripts/transform.sh
+++ build-scripts/transform.sh
@@ -14,10 +14,11 @@ main() {
   echo "Applying transformations..."
 
   ### Audit log
+  echo "Transforming Audit Log docs..."
   api-spec-converter -f openapi_3 -t swagger_2 -c ${docs}/audit > ${docs}/audit.json
 
   ### Authority
-  echo "Authority..."
+  echo "Transforming Authority docs..."
   # The new api gateway lists the authority docs as "Authority" while Baldur lists them as "Core"
   # This "if" check can be removed after Baldur has been removed
   if [ -f "${docs}"/authority ]; then
@@ -25,12 +26,14 @@ main() {
   fi
 
   ### Trusted Activities v2
+  echo "Transforming Trusted Activities docs..."
   # convert openapi 3 yaml to swagger 2 json
   api-spec-converter -f openapi_3 -t swagger_2 -c ${docs}/trusted-activities > $TRUSTED_ACTIVITIES_V2
   rm "${docs}/trusted-activities"
 
   ### File Events
   api-spec-converter -f openapi_3 -t swagger_2 -c ${docs}/file-events > $FILE_EVENTS
+  echo "Transforming File Events docs..."
   # prefix v1 summary fields with "v1" and mark as deprecated
   jq '.paths |= with_entries( if .key | contains("v1") then .value[].summary  |= "v1 - \(.)" else . end)' < $FILE_EVENTS > $TMP && mv $TMP $FILE_EVENTS
   jq '.paths |= with_entries( if .key | contains("v1") then .value[].deprecated |= true else . end)' < $FILE_EVENTS > $TMP && mv $TMP $FILE_EVENTS
@@ -42,6 +45,7 @@ main() {
   mv $FILE_EVENTS ${docs}/file-events
 
   ### Alert Rules v1
+  echo "Transforming Alert Rule v1 docs..."
   # prefix summary fields with "v1"
   jq '.paths[][].summary |= "v1 - \(.)"' < $RULES_V1 > $TMP && mv $TMP $RULES_V1
   # mark v1 endpoints as deprecated
@@ -51,6 +55,7 @@ main() {
   jq '.paths[][].tags[] |= if . == "Rules" then "Alerts" else . end' < $ALERTS > $TMP && mv $TMP $ALERTS
 
   ### Alert Rules v2
+  echo "Transforming Alert Rule v2 docs..."
   # mark rules summary fields with "v2"
   jq '.paths[][].summary |= "v2 - \(.)"' < $RULES_V2 > $TMP && mv $TMP $RULES_V2
   # hide add-users
@@ -61,11 +66,13 @@ main() {
   jq 'del(.paths."/v2/alert-rules/{id}/remove-user-aliases")' < $RULES_V2 > $TMP && mv $TMP $RULES_V2
 
   ## Sessions
+  echo "Transforming Sessions docs..."
   # convert openapi 3 yaml to swagger 2 json
   api-spec-converter -f openapi_3 -t swagger_2 -c ${docs}/sessions > "${docs}/sessions.json"
   rm ${docs}/sessions
 
   ### Watchlists
+  echo "Transforming Department docs..."
   # convert openapi 3 yaml to swagger 2 json
   api-spec-converter -f openapi_3 -t swagger_2 -c ${docs}/departments > "${docs}/departments.json"
   rm ${docs}/departments
@@ -77,15 +84,18 @@ main() {
   rm ${docs}/risk-profiles
 
   # set update-risk-profile PATCH description
+  echo "Transforming Risk Profile docs..."
   jq '.paths."/v2/actor-risk-profiles/{actor_id}".patch.description |= {"$ref": "./api-descriptions/user_risk_profile_patch.rmd"}' < "${docs}/risk-profiles.json" > $TMP && mv $TMP "${docs}/risk-profiles.json"
 
   ### Cases
+  echo "Transforming Cases docs..."
   # convert openapi 3 yaml to swagger 2 json
   api-spec-converter -f openapi_3 -t swagger_2 -c ${docs}/cases > $CASES
   rm ${docs}/cases
   mv $CASES ${docs}/cases
 
   ### Actors
+  echo "Transforming Actor docs..."
   # convert openapi 3 yaml to swagger 2 json
   api-spec-converter -f openapi_3 -t swagger_2 -c ${docs}/actor-enrichment-service > $ACTORS
   rm ${docs}/actor-enrichment-service
